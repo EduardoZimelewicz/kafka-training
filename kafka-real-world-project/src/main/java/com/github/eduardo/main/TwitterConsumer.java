557312ccf609ffe732084b7e91c45646a4144ce3
EduardoZimelewicz <eduardo.zimelewicz@gmail.com>
2020-03-11 19:23:19 -0300

    twitter consumer and kafka consumer added

---
+++ kafka-real-world-project/src/main/java/com/github/eduardo/main/TwitterConsumer.java
@@ -0,0 +1,116 @@
+package com.github.eduardo.main;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.event.Event;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class TwitterConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(TwitterConsumer.class);
+
+    public static void main(String[] args) {
+
+        /**
+         * Kafka producer setup
+         */
+        Properties properties = new Properties();
+
+        String bootstrapServers = "localvm:9092";
+        String topic = "tweet_topic";
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        /**
+         * Twitter client connection setup
+         */
+
+        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+        BlockingQueue<Event> eventQueue = new LinkedBlockingQueue<Event>(1000);
+
+        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+
+        // Optional: set up some followings and track terms
+        List<Long> followings = Lists.newArrayList(1234L, 566788L);
+        List<String> terms = Lists.newArrayList("twitter", "api");
+        hosebirdEndpoint.followings(followings);
+        hosebirdEndpoint.trackTerms(terms);
+
+        String consumerKey = "{{ twitter_consumer_key }}";
+        String consumerSecret = "{{ twitter_consumer_secret }}";
+        String accessToken = "{{ twitter_access_token }}";
+        String accessTokenSecret = "{{ twitter_access_token_secret }}";
+
+        // These secrets should be read from a config file
+        Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue))
+                .eventMessageQueue(eventQueue);                          // optional: use this if you want to process client events
+
+        Client hosebirdClient = builder.build();
+
+        // Attempts to establish a connection.
+        hosebirdClient.connect();
+
+        for (int i=0; i<10; i++) {
+
+            try {
+                String msg = msgQueue.take();
+
+                final ProducerRecord<String, String> record =
+                        new ProducerRecord<String, String>(topic, msg);
+
+                producer.send(record, (recordMetadata, e) -> {
+                    if (e == null) {
+                        logger.info("Received new metadata. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp() + "\n");
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }).get();
+
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        hosebirdClient.stop();
+        producer.flush();
+        producer.close();
+    }
+}
